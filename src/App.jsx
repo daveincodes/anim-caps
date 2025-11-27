f52761baac782e6eb19d093dd5138a8167510474
mindept <david@mindept.com.mx>
2025-11-27 17:32:36 -0600

    anim-footer-height-2

--- src/App.jsx
+++ src/App.jsx
@@ -468,7 +468,7 @@ const App = () => {
                 <RefrigeradoresFarmaceuticos />
               </div>
               
-              <div style={{ position: 'absolute', top: '566.6vh'}}>
+              <div style={{ position: 'absolute', top: '566vh'}}>
                  <Footer />
               </div>
              
